18ab174b98ea3b533068c11498ccc193f438ac7f
Slava Krokhalev <krox1337@yandex.ru>
2019-07-21 23:18:40 +0500

    added base game settings

--- main.py
+++ main.py
@@ -4,23 +4,29 @@ from pygame.locals import *
 
 class Game:
     def __init__(self):
-        pass
+        pygame.display.set_caption('Bombers')
+        self.screen = pygame.display.set_mode((500, 500))
+        self.running = True
     
 
     def handler(self):
-        pass
+        for e in pygame.event.get():
+            if e.type == QUIT:
+                self.running = False
     
 
     def loop(self):
-        pass
+        while self.running:
+            self.handler()
+            self.draw()
 
     
     def draw(self):
         pass
 
     
-    def run(slef):
-        pass
+    def run(self):
+        self.loop()
     
 
     def close(self):
@@ -30,4 +36,5 @@ class Game:
     def pause(self):
         pass
 
-    
+if __name__ == "__main__":
+    g = Game().run()
