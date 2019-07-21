9c565e71e588fc638eda9b750bfa92ffeb8aee65
Slava Krokhalev <krox1337@yandex.ru>
2019-07-21 23:13:12 +0500

    init main class

---
+++ main.py
@@ -0,0 +1,33 @@
+import pygame
+from pygame.locals import *
+
+
+class Game:
+    def __init__(self):
+        pass
+    
+
+    def handler(self):
+        pass
+    
+
+    def loop(self):
+        pass
+
+    
+    def draw(self):
+        pass
+
+    
+    def run(slef):
+        pass
+    
+
+    def close(self):
+        pass
+    
+
+    def pause(self):
+        pass
+
+    
