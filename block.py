faedbaaa372c941e668c9b3833ac1ae761519cd6
Slava <krox1337@yandex.ru>
2019-07-23 16:21:29 +0500

    refactoring (added base_game_object mixin)

--- block.py
+++ block.py
@@ -5,9 +5,5 @@ from pygame.locals import *
 
 class Block(pygame.sprite.Sprite):
     def __init__(self, x, y):
-        pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface((30, 30))
-        self.rect = self.image.get_rect()
-        self.rect.x = x
-        self.rect.y = y
+        base_game_object.BaseGameObject.__init__(self, x, y, all_objects, (30, 30))
         self.image.fill((255, 0, 255))
--- character.py
+++ character.py
@@ -1,15 +1,11 @@
 import pygame
+import base_game_object
 from pygame.locals import *
 
 
-class Character(pygame.sprite.Sprite):
-    def __init__(self, center, all_objects):
-        pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface((30, 30))
-        self.rect = self.image.get_rect()
-        self.rect.center = center
-        self.all_objects = all_objects
-        self.all_objects.add(self)
+class Character(base_game_object.BaseGameObject):
+    def __init__(self, x, y, all_objects):
+        base_game_object.BaseGameObject.__init__(self, x, y, all_objects, (30, 30))
         self.speed = 3
         self.directionx = 0
         self.directiony = 0
